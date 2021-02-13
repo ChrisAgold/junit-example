65febd822aee124a48016576267722eba50db8d0
Chris Agold <christophersagold@gmail.com>
2021-02-13 15:59:48 -0600

    first push

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,92 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class FirstTest {
+
+    private ArrayList<String> names;
+
+    @Before
+    public void setUp(){
+        this.names = new ArrayList<>();
+        this.names.add("Fer");
+    }
+
+    @Test
+    public void testIfCompanyNameIsEquals(){
+        String expected = "Codeup";
+        String actual = "Codeup";
+
+        assertEquals(expected, actual);
+    }
+
+    // When you're using floating points values a third parameter is required (Delta) which represents a positive margin of error of decimals.
+
+    @Test
+    public void testIfChangeIsCorrect() {
+        Double price = 10.0;
+        Double discount = 4.5;
+
+        assertEquals(5.5, price - discount, 0);
+        assertEquals(5.1, price - discount, 0.5);
+        assertNotEquals(4.9, price - discount, 0.5);
+    }
+
+    @Test
+    public void testIfObjectsAreDifferent() {
+        Object dog = new Object();
+        Object sheep = new Object();
+        Object clonnedSheep = new Object();
+
+        assertNotSame(sheep, dog);
+        assertNotSame(sheep, clonnedSheep);
+    }
+
+    @Test
+    public void testIfArrayEquals() {
+        char[] expected = {'J','u','n','i','t'};
+        char[] actual = "Junit".toCharArray();
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testIfGreaterOrLesserThanWorks() {
+
+        boolean learningTDD = true;
+
+        assertTrue(learningTDD);
+        assertTrue("5 is greater than 4", 5 > 4);
+        assertFalse("5 is not greater than 6", 5 > 6);
+    }
+
+    @Test
+    public void testIfInstanceIsNull() {
+        Device phone = new Device();
+        Device laptop = null;
+
+        assertNull(laptop);
+        assertNotNull(phone);
+    }
+
+    // We can verify that the names list is not longer null because it was initialized in the setUp() method with the following test:
+    @Test
+    public void testIfNamesIsInitialized(){
+        assertNotNull(names);
+    }
+
+    @Test
+    public void testIfANameCanBeAdded(){
+        assertEquals(1, this.names.size());
+        this.names.add("Zach");
+        assertEquals(2, this.names.size());
+
+        assertSame("Fer", this.names.get(0));
+        assertSame("Zach", this.names.get(1));
+    }
+
+}
